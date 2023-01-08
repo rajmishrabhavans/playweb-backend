b527c1fa0d5d049ea4c17baad0c49761d5f769d7
Raj Mishra <mrajshivnandan@gmail.com>
2023-01-08 16:14:59 +0530

    cors middleware modified

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ require('./db/conn')  //connect to DB
 const app= express();
 
 const PORT= process.env.PORT || 8000;
-app.use(cors());
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(express.json())  //to get data from user as json format
 app.use(require('./router/auth'));  //all restAPi calls
 
